94ccec52160c3ae5e4b6619e9fd0936727a577cf
luyosamson <luyosamson@gmail.com>
2022-12-05 12:06:20 +0300

    Delete migration

--- db/migrate/20221205085239_create_restaurant_pizzas.rb
+++ db/migrate/20221205085239_create_restaurant_pizzas.rb
@@ -2,6 +2,9 @@ class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurant_pizzas do |t|
 
+       t.integer :price
+      t.string :pizza_id
+      t.integer :restaurant_id
       t.timestamps
     end
   end
