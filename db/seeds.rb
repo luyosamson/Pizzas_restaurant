f04e7ba9eeda1f5ecdcb981e89400ba0ae228486
luyosamson <luyosamson@gmail.com>
2022-12-05 10:16:02 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create([
+    {
+         name: "Pala Hotel",
+         address: "298 Atlantic Ave, Brooklyn, NY 11201" 
+    },
+    {
+         name: "Sott",
+         address: "298 Atlantic Ave, Brooklyn, NY 11201" 
+    },
+    {
+         name: "Sottocasa NYC",
+         address: "298 Atlantic Ave, Brooklyn, NY 11201" 
+    },
+    {
+         name: "Sottocasa NYC",
+         address: "298 Atlantic Ave, Brooklyn, NY 11201" 
+    }
+    ])
+    Pizza.create([
+        { 
+            name: "Cheese", 
+            ingredients: "Dough, Tomato Sauce, Cheese" 
+        },
+        {
+            name: "Pepperoni",
+            ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni" 
+        }
+                ])
+puts "Done Seeding"
