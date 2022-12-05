a36264bb1721cfa2ff67e0e4ec3df8839c232984
luyosamson <luyosamson@gmail.com>
2022-12-05 10:42:02 +0300

    Implement actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantsController < ApplicationController
+    def index
+    restaurant=Restaurant.all
+    render json:restaurant
+    end
+    def show
+        restaurant=Restaurant.find(params[:id])
+        if restaurant.valid?
+            render json: restaurant
+        else
+            render json:{error:"Restaurant Not Found"},status: :not_found
+        end
+    end
+
+    def destroy
+        restaurant=Restaurant.find_by(id: params[:id])
+        if restaurant
+            restaurant.destroy
+            head :no_content
+        else
+            render json:{error:"Restaurant not found"},status: :not_found
+        end
+    end
 end
